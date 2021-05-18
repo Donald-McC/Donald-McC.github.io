469587d41923f643df1b8884d76ab75e7a7ea226
Donald-McC <dormccar@gmail.com>
2021-05-18 19:58:01 -0400

    index update fix
    in main.js  the slidePanel function, panelIndex was never updated, so it only ever displayed home. It now updates to the correct index.

--- scripts/Main.js
+++ scripts/Main.js
@@ -19,18 +19,23 @@ var panelControl = {
 
     },
     slidePanel : function (input) {
+        this.panelIndex = input;
+
         console.log('sliding over panel; input: ' + input);
         let panels = document.getElementsByClassName('panel');
         let navs = document.getElementsByClassName('navButton');
-
+        
+        //These don't apply until we add arrows later --------
         if(input > panels.length){
             this.panelIndex = 0;
         }
         if(input < 0){
             this.panelIndex = panels.length - 1;
         }
+        //  --------------------------------------------------
 
         for(let x = 0; x < panels.length; x++){
+            console.log('x: ' + x);
             panels[x].style.display = 'none';
             navs[x].style = hidden
         }
